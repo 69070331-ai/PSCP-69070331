5bec3396802b39ab7b731cdab6d4a2d1a05f370c
69070331-ai <69070331@kmitl.ac.th>
2026-09-03 00:52:42 +0700

    Add FizzBuzz implementation in FizzBuzz.py

---
+++ 3167/FizzBuzz.py
@@ -0,0 +1,12 @@
+"FizzBuzz"
+N = int(input())
+
+for i in range(1,N+1):
+    if  not i % 3 and not i % 5 :
+        print("FizzBuzz")
+    elif not i % 3:
+        print("Fizz")
+    elif not i % 5:
+        print("Buzz")
+    else:
+        print(i)
