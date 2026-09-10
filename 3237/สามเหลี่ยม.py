2c250067b6b63f00b592e82a70b2fe8d47f0024a
69070331-ai <69070331@kmitl.ac.th>
2026-09-10 22:29:01 +0700

    Create สามเหลี่ยม.py

---
+++ 3237/สามเหลี่ยม.py
@@ -0,0 +1,10 @@
+"สามเหลี่ยม"
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        if j == 1 or i == n or i == j:
+            print(0, end="")
+        else:
+            print(1, end="")
+    print()
